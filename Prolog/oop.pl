67fa87132a384fd62e4d0c2ce6f53ebe8c35e4bb
davidenicoletta <davidenicoletta@penguin>
2023-12-13 19:43:19 +0100

    Creazioe def_class

--- Prolog/oop.pl
+++ Prolog/oop.pl
@@ -2,19 +2,24 @@
 %%%% 858101 Nicoletta Davide
 
 %%fields/2, fields/3
-fields(X, Y):-
+%fields(_, _).
    % assert(field(X, Y)).
 
-fields(X, Y, Z):-
-    %assert(field(X, Y, Z)).
+%fields(_, _, _).
+%assert(field(X, Y, Z)).
+is_field([]).
+is_field(field(_, _)).
+is_field(field(_, _, _)).
+is_field([X | Rest]):-
+    is_field(X),
+    is_field(Rest).
 
 %% def_class/2, def_class/3
 def_class(_, []).
 def_class(_, [_]).
 def_class(X, [], Z):-
-    is_list(Z),
-    %aggiungere is_fields
-    assert(class(X, Z)).
+    is_field(Z),
+    assert(class(X, [], Z)).
 def_class(_, [_], [_]).
     
 %%%% end of file -- oop.pl    
